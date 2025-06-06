5fd3f961b82428bd0fef9f70988fbc6e0d9f155e
David D. <runfromnuke@gmail.com>
2025-06-06 11:28:18 -0500

    feat:  initial calss to handle html conversion (#1)
    * feat: add bare bones class to handle html elements
    
    This isn't quite done, hence the branch.  Pick up where you left off.
    
    * feat: add basic string formatting for html
    
    Fill out the very basic string formatting to taking a dictionary of
    "props" and converting them to HTML attributes for the associated tag.

---
+++ src/htmlnode.py
@@ -0,0 +1,21 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        pass
+        attribs = list()
+
+        for key in self.props:
+            attribs.append(f' {key}="{self.props[key]}"')
+
+        return "".join(attribs)
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
