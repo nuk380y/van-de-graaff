b81d51ebc18e9df20a295dbec511649e8b7f529f
David Dean <runfromnuke@gmail.com>
2025-06-08 15:13:41 -0500

    refactor: adopt cleaner formatting of assertions
    Finally realized I can have the two arguments of any of my `assert`
    statements spread across multiple lines.  This just looks cleaner.
    
    Also caught I typo in my transposition and corrected that here, too.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -6,36 +6,44 @@ from htmlnode import HTMLNode, LeafNode
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode("p", "This is only a test", None, {"color": "ffffff"})
-        expected_repr = (
-            "HTMLNode('p', 'This is only a test', None, {'color': 'ffffff'})"
+        self.assertEqual(
+            f"{node}",
+            "HTMLNode('p', 'This is only a test', None, {'color': 'ffffff'})",
         )
-        self.assertEqual(f"{node}", f"{expected_repr}")
 
     def test_to_html(self):
         node = HTMLNode("p", "This is only a test", None)
-        expected_repr = "HTMLNode('p', 'This is only a test', None, None)"
-        self.assertEqual(f"{node}", f"{expected_repr}")
+        self.assertEqual(
+            f"{node}",
+            "HTMLNode('p', 'This is only a test', None, None)",
+        )
         with self.assertRaises(NotImplementedError):
             node.to_html()
 
     def test_props_to_html(self):
         attribs = {"href": "https://pypi.org", "rel": "noopener nofollower"}
         node = HTMLNode("a", "Example link", None, attribs)
-        expected_props = ' href="https://pypi.org" rel="noopener nofollower"'
-        self.assertEqual(f"{node.props_to_html()}", f"{expected_props}")
+        self.assertEqual(
+            f"{node.props_to_html()}",
+            ' href="https://pypi.org" rel="noopener nofollower"',
+        )
 
 
 class TestLeafNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
-        expected = "<p>Hello, world!</p>"
-        self.assertEqual(node.to_html(), expected)
+        self.assertEqual(
+            node.to_html(),
+            "<p>Hello, world!</p>",
+        )
 
     def test_leaf_to_html_a(self):
         attribs = {"href": "https://pypi.org"}
         node = LeafNode("a", "Python Package Index", attribs)
-        expected = '<a href="https://pypi.org">Python Package Index</a>'
-        self.assertEqual(node.to_html(), expected)
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://pypi.org">Python Package Index</a>',
+        )
 
     def test_leaf_to_html_error(self):
         node = LeafNode("p", None)
@@ -45,7 +53,7 @@ class TestLeafNode(unittest.TestCase):
 
 class TestParentNode(unittest.TestCase):
     def test_to_html_with_children(self):
-        child_node = LeadNode("span", "child")
+        child_node = LeafNode("span", "child")
         parent_node = ParentNode("div", [child_node])
         self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
 
