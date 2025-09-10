559867a9bb744e59553e5a226a46ec147d63228c
nuk380y <runfromnuke@gmail.com>
2025-09-10 15:01:54 -0500

    test: add test to confirm not equals operations
    Added a test to confirm that not equal (`!=`) operations behave as
    expected.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
         self.assertEqual(node, node2)
 
+    def test_not_eq(self):
+        node = TextNode("This is node", TextType.PLAIN_TEXT)
+        node2 = TextNode("This is node2", TextType.PLAIN_TEXT)
+        self.assertNotEqual(node, node2)
+
     def test_repr(self):
         node = TextNode("Test text please ignore", TextType.PLAIN_TEXT)
         exp_repr = 'TextNode("Test text please ignore", , None)'
