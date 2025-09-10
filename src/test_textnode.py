332a6fa62d01e31f612092c7fea1c49c66e1a141
David Dean <runfromnuke@gmail.com>
2025-09-09 19:35:09 -0500

    test: add initial test for TextNode __repr__
    Need to modify this so it's verifying that string values are more easily
    recognized.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
         self.assertEqual(node, node2)
 
+    def test_repr(self):
+        node = TextNode("Test text please ignore", TextType.PLAIN_TEXT)
+        exp_repr = "TextNode(Test text please ignore, , None)"
+        self.assertEqual(repr(node), exp_repr)
+
 
 if __name__ == "__main__":
     unittest.main()
