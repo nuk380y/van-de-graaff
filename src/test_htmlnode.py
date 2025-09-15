48edb805deaec4172eea9e1043840b7be26f0deb
David Dean <runfromnuke@gmail.com>
2025-09-15 18:02:08 -0500

    fix: add import to test new function
    I forgot to import the funciton that we're testing...
    
    Simple error with a simple fix.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import HTMLNode, LeafNode, ParentNode, text_node_to_html_node
 from textnode import TextNode, TextType
 
 
