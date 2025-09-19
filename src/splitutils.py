ff7e12e715ac2107ce18d1189713b9961d3b8538
nuk380y <runfromnuke@gmail.com>
2025-09-19 14:35:36 -0500

    feat(STUB): add logic for splitting out md images
    Functionality for splitting out markdown (`md`) formatted images into
    `TextNode`s.  Will be used to as a basis for splitting out links as
    well.

--- src/splitutils.py
+++ src/splitutils.py
@@ -1,3 +1,4 @@
+from extractutils import extract_markdown_images
 from textnode import TextNode, TextType
 
 
@@ -20,3 +21,26 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     new_nodes.append(TextNode(value, node.text_type))
 
     return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN_TEXT:
+            new_nodes.append(node)
+        else:
+            fragments = extract_markdown_images(node)
+
+            if len(fragments) % 2 == 0:
+                raise SyntaxError("unmatched delimiter")
+
+            print(f"{fragments}")
+            # if fragments[0] == "":
+            #     continue
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    pass
